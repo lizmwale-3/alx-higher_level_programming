1643c8a611df35458135ca0c2643a8bae4330fa8
lizmwale-3 <elizabethmwale3@gmail.com>
2022-07-10 02:43:30 +0300

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,4 @@
-#include "lists.h"
+#include "list.h"
 /**
- * check_cycle - checks of a singly linked lists has a cycle
- * @list: head of linked list
- * Return: 0 if cycle is absent, 1 if cycle is present
- **/
-
-int check_cycle(listint_t *list)
-{
-        listint_t *f = NULL, *s = NULL;
-
-        if (!list)
-                return (0);
-        f = list->next;
-        s = list;
-
-        while (f && f->next)
-        {
-                if (f == s)
-                        return (1);
-                f = f->next->next;
-                s = s->next;
-        }
-        return (0);
-}
+ * check_cycle - check whether singly linked lists have cycles
+ * @
